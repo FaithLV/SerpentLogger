93616b8a2510e611900257567f29e7739171a516
Valters <infinity1188@yahoo.com>
2019-03-18 20:56:47 +0200

    Implement Unit Tests for severity range checking

--- SerpentTests/LoggerTests.cs
+++ SerpentTests/LoggerTests.cs
@@ -2,20 +2,77 @@ using System;
 using Xunit;
 using SerpentLogger;
 using SerpentAPI.Models;
+using SerpentAPI.Enums;
+using System.Linq;
 
 namespace SerpentTests
 {
-    public class UnitTest1
+    public class LoggerTests
     {
+
         [Fact]
-        public void TestRecordIsRecorded()
+        public void PassRecordWhenSeverityHigherThanMinimum()
         {
             var serpent = new SerpentKernel();
-            var newEntry = new SerpentEntry("test", EntrySeverity.High);
+            var newEntry = new SerpentEntry(String.Empty, EntrySeverity.Critical);
 
+            serpent.MinimumSeverity = EntrySeverity.High;
             serpent.Record(newEntry);
 
             var records = serpent.GetRecords();
+            var recordCount = records.Count();
+            var isRecorded = recordCount == 1;
+
+            Assert.True(isRecorded);
+        }
+
+        [Fact]
+        public void FailRecordWhenSeverityLowerThanMinimum()
+        {
+            var serpent = new SerpentKernel();
+            var newEntry = new SerpentEntry(String.Empty, EntrySeverity.Medium);
+
+            serpent.MinimumSeverity = EntrySeverity.High;
+            serpent.Record(newEntry);
+
+            var records = serpent.GetRecords();
+            var recordCount = records.Count();
+            var isRecorded = recordCount == 1;
+
+
+            Assert.False(isRecorded);
+        }
+
+        [Fact]
+        public void PassRecordWhenSeverityLowerThanMaximum()
+        {
+            var serpent = new SerpentKernel();
+            var newEntry = new SerpentEntry(String.Empty, EntrySeverity.Medium);
+
+            serpent.MaximumSeverity = EntrySeverity.High;
+            serpent.Record(newEntry);
+
+            var records = serpent.GetRecords();
+            var recordCount = records.Count();
+            var isRecorded = recordCount == 1;
+
+            Assert.True(isRecorded);
+        }
+
+        [Fact]
+        public void FailRecordWhenSeverityHigherThanMaximum()
+        {
+            var serpent = new SerpentKernel();
+            var newEntry = new SerpentEntry(String.Empty, EntrySeverity.Critical);
+
+            serpent.MaximumSeverity = EntrySeverity.High;
+            serpent.Record(newEntry);
+
+            var records = serpent.GetRecords();
+            var recordCount = records.Count();
+            var isRecorded = recordCount == 1;
+
+            Assert.False(isRecorded);
         }
     }
 }
