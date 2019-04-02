b5cc7e3dd0aee52eb11132406e3d83a54a3f5daa
FaithLV <infinity1188@yahoo.com>
2019-04-02 12:51:55 +0300

    Implement console output

---
+++ SerpentLogger/Services/ConsoleOutput.cs
@@ -0,0 +1,21 @@
+using SerpentAPI.Common.Serialization;
+using SerpentAPI.Interfaces;
+
+namespace SerpentLogger.Services
+{
+    public class ConsoleOutput : IDirectOutput
+    {
+        private readonly EntryStringSerializer _serializer;
+
+        public ConsoleOutput(EntryStringSerializer serializer)
+        {
+            _serializer = serializer;
+        }
+
+        public void WriteEntryLine(ISerpentEntry entry)
+        {
+            var serialized = _serializer.Serialize(entry);
+            System.Console.WriteLine(serialized);
+        }
+    }
+}
