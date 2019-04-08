62637201b45f27d4a189fab3b8e9cf5fd00e41c2
FaithLV <infinity1188@yahoo.com>
2019-04-08 17:08:05 +0300

    Clean nuget artifacts on build

--- generate-nuget.sh
+++ generate-nuget.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 dotnet pack --include-source --include-symbols -c Release --output nupkgs
+
 mkdir bin/nupkgs
+
+# Delete previous package builds
+rm bin/nupkgs/*
+
+# Copy latests packages 
 cp SerpentAPI/nupkgs/* bin/nupkgs -v
 cp SerpentKernel/nupkgs/* bin/nupkgs -v
 cp SerpentLogger/nupkgs/* bin/nupkgs -v
+
+# Clear build artifacts
+rm SerpentAPI/nupkgs/*
+rm SerpentKernel/nupkgs/* 
+rm SerpentLogger/nupkgs/*
